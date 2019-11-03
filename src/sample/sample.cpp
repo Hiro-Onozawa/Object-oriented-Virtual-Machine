4fc371db12c124fe28140ca9c72c10f3c81686ee
Hiro-Onozawa <48467312+Hiro-Onozawa@users.noreply.github.com>
2019-11-03 13:54:31 +0900

    サンプルを更新
    TD4で乗除算するプログラムを追加

--- src/sample/sample.cpp
+++ src/sample/sample.cpp
@@ -1,4 +1,5 @@
-#define USE_SIMPLE_ACCUMULATOR
+// #define USE_SIMPLE_ACCUMULATOR
+#define USE_TD4
 
 #ifdef USE_SIMPLE_ACCUMULATOR
 #include "../core/common.h"
@@ -22,3 +23,42 @@ int main(void) {
 	return 0;
 }
 #endif /* USE_SIMPLE_ACCUMULATOR */
+
+#ifdef USE_TD4
+#include "../core/common.h"
+#include <iostream>
+#include <Windows.h>
+#include "../processor/TD4/Instructions.h"
+#include "../processor/TD4/Processor.h"
+
+int main(void) {
+	OVM::Byte m = 2;
+	OVM::Byte n = 3;
+	OVM::Byte in = 5;
+
+	TD4::Processor proc(16);
+	proc.setInstruction(0x0, new TD4::InB());
+	proc.setInstruction(0x1, new TD4::AddAIm(1));
+	proc.setInstruction(0x2, new TD4::AddBIm(1));
+	proc.setInstruction(0x3, new TD4::Jnc(1));
+	proc.setInstruction(0x4, new TD4::AddAIm(0xF));
+	proc.setInstruction(0x5, new TD4::AddAIm(m));
+	proc.setInstruction(0x6, new TD4::Jnc(8));
+	proc.setInstruction(0x7, new TD4::Jmp(0xA));
+	proc.setInstruction(0x8, new TD4::AddBIm(n));
+	proc.setInstruction(0x9, new TD4::Jmp(5));
+	proc.setInstruction(0xA, new TD4::OutB());
+//	proc.setInstruction(0xB, new TD4::Jmp(0xB));
+	proc.setIn(in);
+
+	std::cout << "Input : "
+		<< "m = " << static_cast<uint32_t>(m) << ", "
+		<< "n = " << static_cast<uint32_t>(n) << ", "
+		<< "IN = " << static_cast<uint32_t>(in) << std::endl;
+	while (proc.step()) Sleep(80);
+
+	std::cout << "Output : " << static_cast<uint32_t>(proc.getOut()) << std::endl;
+
+	return 0;
+}
+#endif /* USE_TD4 */
