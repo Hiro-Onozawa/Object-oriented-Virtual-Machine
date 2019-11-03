f297c0ff06d6c4e8ae34efaeaffdbfbcfc6d7b09
Hiro-Onozawa <48467312+Hiro-Onozawa@users.noreply.github.com>
2019-11-03 11:31:04 +0900

    サンプルを更新

--- src/sample/sample.cpp
+++ src/sample/sample.cpp
@@ -1,20 +1,24 @@
+#define USE_SIMPLE_ACCUMULATOR
+
+#ifdef USE_SIMPLE_ACCUMULATOR
 #include "../core/common.h"
-#include "../core/Instruction.h"
-#include "../core/Processor.h"
+#include "../processor/simple-accumulator-machine/Instructions.h"
+#include "../processor/simple-accumulator-machine/SimpleAccumulator.h"
 
 int main(void) {
-	Processor proc(1024, 1024, 32);
-	proc.setInstruction(1, new Load(100));
-	proc.setInstruction(2, new Push());
-	proc.setInstruction(3, new Pop());
-	proc.setInstruction(4, new Print());
-	proc.setInstruction(5, new Add(static_cast<OVM::Register>(-1)));
-	proc.setInstruction(6, new Push());
-	proc.setInstruction(7, new Jez(10));
-	proc.setInstruction(8, new Load(0));
-	proc.setInstruction(9, new Jez(3));
+	SimpleAccumulator::Processor proc(1024, 1024, 32);
+	proc.setInstruction(1, new SimpleAccumulator::Load(100));
+	proc.setInstruction(2, new SimpleAccumulator::Push());
+	proc.setInstruction(3, new SimpleAccumulator::Pop());
+	proc.setInstruction(4, new SimpleAccumulator::Print());
+	proc.setInstruction(5, new SimpleAccumulator::Add(static_cast<OVM::Register>(-1)));
+	proc.setInstruction(6, new SimpleAccumulator::Push());
+	proc.setInstruction(7, new SimpleAccumulator::Jez(10));
+	proc.setInstruction(8, new SimpleAccumulator::Load(0));
+	proc.setInstruction(9, new SimpleAccumulator::Jez(3));
 	
 	while(proc.step()) ;
 	
 	return 0;
 }
+#endif /* USE_SIMPLE_ACCUMULATOR */
